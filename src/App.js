b3289525ca52f4ccefd83a77134a7bcac88b5b37
Samar AYADI <ayadisamardev@gmail.com>
2023-09-26 14:51:49 +0100

    First component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-
+import MyFirstComponent from './MyFirstComponent';
 function App() {
   return (
     <div className="App">
@@ -9,7 +9,9 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <h1>PinguCoder</h1>
+        <MyFirstComponent />
+        <MyFirstComponent />
+        <MyFirstComponent />
         <a
           className="App-link"
           href="https://reactjs.org"
