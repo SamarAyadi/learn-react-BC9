03f181b64617f03cd3220cb34dfb81b99cf3572c
Samar AYADI <ayadisamardev@gmail.com>
2023-09-26 15:21:26 +0100

    Adding Style to JSX elements

--- src/MyFirstComponent.js
+++ src/MyFirstComponent.js
@@ -3,17 +3,35 @@ function MyFirstComponent() {
     name: "Samar",
     email: "we@lota.me",
   };
+
+  const elemStyle = {
+    backgroundColor: "red",
+    fontSize: "20px",
+    boxShadow: "0px 20px 10px black",
+  };
   return (
     <>
       <div>
-        <h1>{person.name}</h1>
-        <h3>{person.email}</h3>
-        <h2>hi</h2>
-        <button>Click Me</button>
+        <h1
+          style={{
+            backgroundColor: "blue",
+            fontSize: "20px",
+            boxShadow: "0px 20px 10px black",
+          }}
+        >
+          {person.name}
+        </h1>
+        <h3 style={{ backgroundColor: "green" }}>{person.email}</h3>
+        <h2 style={elemStyle}>hi</h2>
+        <button onClick={sayHello}>Click Me</button>
       </div>
       <div></div>
     </>
   );
+}
+
+function sayHello() {
+  alert("Hello");
 }
 
 export default MyFirstComponent;
